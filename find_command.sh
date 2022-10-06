3ff490e5cbeca73bcec1625810058e36c6d84921
Sam Cole <113649989+sweatycoodu@users.noreply.github.com>
2022-10-07 10:55:04 +1100

    Folder Tree Cleanup

--- find_command.sh
+++
@@ -1,9 +0,0 @@
-# Script / Instructions for KodeKloud Engineer Task: Linux Find Command.
-# Written by Sam Cole - https://github.com/sweatycoodu/
-# As always - give it a go first - use if stuck.
-
-# Verify the requested .js files can be found in /var/www/html/blog/.
-sudo find /var/www/html/blog/ -name "*.js"
-
-# Copy those .js files into the /blog location.
-sudo find /var/www/html/blog/ -name "*.js" -exec cp --parent {} /blog \;
--- user_ni_shell.sh
+++
@@ -1,9 +0,0 @@
-# Script / Instructions for KodeKloud Engineer Task: Create a user with a non-interactive shell.
-# Written by Sam Cole - https://github.com/sweatycoodu/
-# As always - give it a go first - use if stuck.
-
-# Create a user called ammar with a non-interactive shell.
-sudo useradd -s /sbin/nologin ammar
-
-# Verify the user was created and has a non-interactive shell.
-cat /etc/passwd | grep ammar | cut -d: -f7
