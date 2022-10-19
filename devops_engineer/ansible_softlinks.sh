ad07737497e6dc7ed38b69cba32fb15877ec72e1
Sam Cole <113649989+sweatycoodu@users.noreply.github.com>
2022-10-19 11:43:01 +1100

    Create ansible_softlinks.sh and yml files.

---
+++ devops_engineer/ansible_softlinks.sh
@@ -0,0 +1,16 @@
+# Instructions for KodeKloud Engineer Task: Creating Soft Links Using Ansible.
+# Written by Sam Cole - https://github.com/sweatycoodu/
+# As always - give it a go first - use if stuck.
+
+# Write the YAML code as per specifications.
+# Refer to my file if you get really stuck.
+
+# Copy your YAML into the playbook.yml file.
+touch playbook.yml
+vi playbook.yml
+
+# Run the playbook.
+ansible-playbook -i inventory playbook.yml
+
+# Confirm the output shows success on all tasks.
+# Review your YAML if there are any errors.
