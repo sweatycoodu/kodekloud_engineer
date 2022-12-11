2bc50d3a00fa5028600f0bfad5af724f91e8a14f
Sam Cole <sam.cole@tealium.com>
2022-12-12 10:59:07 +1100

    Create ansible_conditionals.sh and yml code.

---
+++ devops_engineer/ansible_conditionals.sh
@@ -0,0 +1,13 @@
+# Instructions for KodeKloud Engineer Task: Using Ansible Conditionals.
+# Written by Sam Cole - https://github.com/sweatycoodu/
+# As always - give it a go first - use if stuck.
+
+# Create the playbook file.
+touch /home/thor/ansible/playbook.yml
+
+# Edit the playbook file. Refer to ansible_conditionals.yml for the code.
+vi /home/thor/ansible/playbook.yml
+
+# Run the playbook. Inventory file is located at /home/thor/ansible.
+ansible-playbook /home/thor/ansible/playbook.yml -i /home/thor/ansible
+
