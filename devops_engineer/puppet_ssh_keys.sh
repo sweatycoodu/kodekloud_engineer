3fe30a5fc06f32fde96934dea456bc22e531996f
Sam Cole <sam.cole@tealium.com>
2022-12-12 12:43:49 +1100

    Create puppet_ssh_keys.sh

---
+++ devops_engineer/puppet_ssh_keys.sh
@@ -0,0 +1,6 @@
+# Instructions for KodeKloud Engineer Task: Puppet Setup SSH Keys.
+# Written by Sam Cole - https://github.com/sweatycoodu/
+# As always - give it a go first - use if stuck.
+
+# Switch to root user.
+sudo su
