bd43b3bd804b95473d638c5823a230c6fd1ce652
Sam Cole <samjcole08@gmail.com>
2022-12-16 10:12:56 +1100

    Create ec2 lab and k8's redis configs

---
+++ devops_engineer/kubernetes_redis_deploy.sh
@@ -0,0 +1,25 @@
+# Instructions for KodeKloud Engineer Task:  Kubernetes Redis Deployment.
+# Written by Sam Cole - https://github.com/sweatycoodu/
+# As always - give it a go first - use if stuck.
+
+# Switch to the root user.
+sudo su
+
+# Create a new directory for the deployment files.
+mkdir redis-deployment && cd redis-deployment
+
+# Create a configuration file for the redis deployment.
+touch my-redis.config.yml
+vi my-redis.config.yml # Populate the file with the data from the .yml file in the repo.
+
+# Create a deployment file for the redis deployment.
+touch redis-deployment.yml
+vi redis-deployment.yml # Populate the file with the data from the .yml file in the repo.
+
+# Quit root and apply the configmap and deployment files.
+exit
+kubectl apply -f my-redis.config.yml -f redis-deployment.yml
+
+# Check container status.
+kubectl get pods # Should show 1/1 for the redis container with no issues.
+
