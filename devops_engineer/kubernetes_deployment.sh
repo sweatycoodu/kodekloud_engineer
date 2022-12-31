79ee5f1e5f44bea51734a2eb27d6c5974368d9f4
S. Cole <113649989+sweatycoodu@users.noreply.github.com>
2022-12-31 01:50:34 +0000

    Code changes

--- devops_engineer/kubernetes_deployment.sh
+++ devops_engineer/kubernetes_deployment.sh
@@ -6,8 +6,10 @@ sudo su
 # Create a new namespace.
 kubectl create namespace devops
 
-# Create a new deployment.
-kubectl create deployment devops --image=devops_engineer:latest -n devops
+# Create the directory and file for the kubernetes manifest.
+mkdir -p /root/devops_engineer/kubernetes && cd /root/devops_engineer/kubernetes && touch deployment.yaml
+
+# Add the following content to the deployment.yaml file.
 
 
 
