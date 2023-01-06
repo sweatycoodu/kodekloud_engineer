1bc2042a2ec0d6af95801f7ec1cc1026d7b07075
Sam Cole <sam.cole@tealium.com>
2023-01-06 11:50:22 +1100

    Tidy-up.

--- acg_labs/cka_practice_01.sh
+++ acg_labs/cka_practice_01.sh
@@ -8,8 +8,6 @@ echo 2 > /k8s/0001/count.txt
 # In the backend namespace, check the log for the proc container in the data-handler Pod.
 kubectl logs -n backend data-handler -c proc | grep "ERROR" > /k8s/0002/error.txt
 
-
-
 # Analyse the pod in the web namespace with the label app=auth that are consuming the most CPU usage.
 # Output the name of the pod to a file.
 kubectl top pod -n web -l app=auth --sort-by=cpu | head -n 2 | tail -n 1 | awk '{print $1}' > /k8s/0003/cpu-pod.txt
